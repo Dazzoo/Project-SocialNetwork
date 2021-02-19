94461d2307b1ed9aef791c22c6c0ab12aef3fdc5
Dazzoo <yuraks46@gmail.com>
2021-02-19 23:36:54 +0200

    Split into components in messages. Need to do open dialogs after click on name

--- my-app/src/Components/Messages/Messages.js
+++ my-app/src/Components/Messages/Messages.js
@@ -1,45 +1,71 @@
 import c from './Messages.module.css'
+import '../../App.css';
 import {NavLink} from "react-router-dom"
+import {BrowserRouter, Route} from "react-router-dom";
+
+
+const Dialog = (props) => {
+    return (
+        <div className={c.dialog}>
+            <NavLink to={"/messages/" + props.id}>{props.name}</NavLink>
+        </div>
+    );
+
+};
+
+const Text = (props) => {
+    return (
+        <div className={c.text}>
+            {props.lastMessage}
+        </div>
+    );
+}
+
+const LastMessage = (props) => {
+    return (
+        <div className={c.lastMessages}>
+            <Text lastMessage="Hi" />
+            <Text lastMessage="How do you do bro?" />
+            <Text lastMessage="I would like more, heh)" />
+            <Text lastMessage="Nope, its bad idea." />
+            <Text lastMessage="I am from Taiwan" />
+            <Text lastMessage="Here we go again" />
+        </div>
+
+    );
+}
+
+const UserDialog = (props) => {
+    return (
+        <div>
+            {props.text}
+        </div>
+
+    );
+}
+
 
 const Messages = () => {
     return (
-        <div className={c.messages}>
-            <div className={c.dialogs}>
-                <div className={c.dialog}>
-                    <NavLink to="/messages/1">Monica</NavLink>
-                </div>
-                <div className={c.dialog}>
-                    <NavLink to="/messages/2">Artem</NavLink>
-                </div>
-                <div className={c.dialog}>
-                    <NavLink to="/messages/3">Nasty</NavLink>
-                </div>
-                <div className={c.dialog}>
-                    <NavLink to="/messages/4">Nika</NavLink>
-                </div>
-                <div className={c.dialog}>
-                    <NavLink to="/messages/5">Andrey</NavLink>
-                </div>
-                <div className={c.dialog}>
-                    <NavLink to="/messages/6">Oliver</NavLink>
-                </div>
+        <BrowserRouter>
+        <div className={c.dialogs}>
+            <div className={c.dialogsItems}>
+                <Dialog name="Monica" id="1"/>
+                <Dialog name="Artem" id="2"/>
+                <Dialog name="Nasty" id="3"/>
+                <Dialog name="Nika" id="4"/>
+                <Dialog name="Andrey" id="5"/>
+                <Dialog name="Oliver" id="6"/>
             </div>
-            <div className={c.messagesText}>
-                <div className={c.messageText}>
-                    Hi
-                </div>
-                <div className={c.messageText}>
-                    How do you do bro?
-                </div>
-                <div className={c.messageText}>
-                    I would like more, heh)
-                </div>
-                <div className={c.messageText}>
-                    Nope, its bad idea.
-                </div>
-
-            </div>
+            <Route path='/messages' component={LastMessage}/>
+            <Route path='/messages/1' component={UserDialog}/>
+            <Route path='/messages/2' component={UserDialog}/>
+            <Route path='/messages/3' component={UserDialog}/>   {/* Сделать открытие всей переписки с человеком по клику ника */}
+            <Route path='/messages/4' component={UserDialog}/>
+            <Route path='/messages/5' component={UserDialog}/>
+            <Route path='/messages/6' component={UserDialog}/>
         </div>
+        </BrowserRouter>
     );
 }
 
