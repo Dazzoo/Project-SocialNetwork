1e80a4a360d21ae802ec13319608a7668e14d2e7
DazoTT <yuraks46@gmail.com>
2021-03-09 22:11:21 +0200

    using Class Component in findUsers

--- my-app/src/Components/findUsers/findUsers.js
+++ my-app/src/Components/findUsers/findUsers.js
@@ -3,39 +3,40 @@ import c from './findUsers.module.css';
 import * as axios from 'axios';
 
 
-const findUsers = (props) => {
-            if(props.users.length === 0){
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {props.SetUsers(response.data.items)});
-        }
+class findUsers extends React.Component {
+    constructor(props) {
+        super(props);
 
-    debugger
-
-    const UsersShow = props.users.map( u =>
-           <div className={c.wrapper}>
-                <div className={c.inlineBlock}>
-                    <div className={c.profileImage}>
-                        <img src={u.photos.small != null ? u.photos.small : 'https://www.uniprep.cz/sites/default/files/public/pictures/picture-51-1423427108.png'}/>
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.SetUsers(response.data.items)})
+    }
+    render() {
+        return (
+            <div>
+                {this.props.users.map( u =>
+                <div key={u.id} className={c.wrapper}>
+                    <div className={c.inlineBlock}>
+                        <div className={c.profileImage}>
+                            <img src={u.photos.small != null ? u.photos.small : 'https://www.uniprep.cz/sites/default/files/public/pictures/picture-51-1423427108.png'}/>
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => {this.props.Follow(u.id)}}>Unfollow</button>
+                                : <button onClick={() => {this.props.Unfollow(u.id)}}>Follow</button>
+                            }
+                        </div>
                     </div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={() => {props.Follow(u.id)}}>Follow</button>
-                            : <button onClick={() => {props.Unfollow(u.id)}}>Unfollow</button>
-                        }
+                    <div className={c.inlineBlock}>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
                     </div>
                 </div>
-                <div className={c.inlineBlock}>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </div>
+            )
+                }
             </div>
-
-    )
-
-    return (
-        <div>
-            {UsersShow}
-        </div>
-    );
+        )
+    }
 }
 
 
