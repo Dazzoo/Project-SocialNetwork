4b09698aa86baef4e1325d02b37b20119f1f2755
DazoTT <yuraks46@gmail.com>
2021-04-07 00:33:40 +0300

    functionality of edit profile functionality 1/2

--- my-app/src/Components/MyProfile/Profile/Profile.js
+++ my-app/src/Components/MyProfile/Profile/Profile.js
@@ -7,6 +7,7 @@ import FetchingIcon from '../../findUsers/FetchingIcon/FetchingIcon'
 const Profile = (props) => {
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status)
+    let [editModeProfile, setEditModeProfile] = useState(false)
 
     useEffect(() => {
         setStatus(props.status)
@@ -34,17 +35,56 @@ const Profile = (props) => {
         )
     }
 
+    const SwitchEditModeProfile = (e) => {
+        if(!editModeProfile){
+            setEditModeProfile(true)
+        }
+        if(editModeProfile){
+            setEditModeProfile(false)
+        }
+    }
+
 
     return (
         <div className={style.Avatar}>
-            <img src={props.profile.photos.large ? props.profile.photos.large : 'https://www.uniprep.cz/sites/default/files/public/pictures/picture-51-1423427108.png'}/>
-            {props.isOwner? <input type="file" className={style.customFileInput} onChange={changeProfilePhoto} /> : null }
-            <div>{props.profile.fullName ? 'FullName: ' + props.profile.fullName : null}</div>
-            <div>{props.profile.aboutMe ? 'About me: ' + props.profile.aboutMe : null}</div>
-            <div>{props.profile.aboutMe ? <div>lookingForAJob : yes</div> : <div>lookingForAJob : no</div>}</div>
-            {editMode === false ?
-                <span className={style.status} onClick={ActivateEditMode} >{status ? status : '____________________'}</span> :
-                <input onChange={OnStatusChange} value={status} onBlur={DeactivateEditMode} autoFocus={true} />}
+            <img src={props.profile.photos.large ? props.profile.photos.large
+                :
+                'https://www.uniprep.cz/sites/default/files/public/pictures/picture-51-1423427108.png'}/>
+            {props.isOwner? <input type="file" className={style.customFileInput} onChange={changeProfilePhoto} />
+            :
+                null }
+            <button onClick={SwitchEditModeProfile} >{editModeProfile? 'save'
+                :
+                'EditInfo' }</button>
+            {editModeProfile ?
+
+                <div>
+
+                </div>
+
+                :
+
+                <div>
+                <div>{props.profile.fullName ? 'FullName: ' + props.profile.fullName : null}</div>
+                <div>{props.profile.aboutMe ? 'About me: ' + props.profile.aboutMe : null}</div>
+                <div>{props.profile.lookingForAJob ? 'lookingForAJob : ' + props.profile.lookingForAJobDescription
+                    :
+                    null}
+                    <div>{Object.keys(props.profile.contacts).map(key => {
+                        // return <Contact key={key} contactTitle={key} contactValue={props.profile.contacts(key} />
+                    })}</div>
+                </div>
+                </div>
+            }
+            {editModeProfile ?
+                null
+                :
+                (!editMode ?
+                <span className={style.status} onClick={ActivateEditMode} >{status ? status : '____________________'}</span>
+                    :
+                <input onChange={OnStatusChange} value={status} onBlur={DeactivateEditMode} autoFocus={true} />)
+            }
+
         </div>
     )
 }
