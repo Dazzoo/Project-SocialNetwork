29aa1ff156e88e2a3573e0eb3a0896e465f3eb86
DazoTT <yuraks46@gmail.com>
2021-04-19 19:00:15 +0300

    ...

--- my-app/src/Components/MyNews/MyNews.js
+++ my-app/src/Components/MyNews/MyNews.js
@@ -1,9 +1,13 @@
 import c from './MyNews.module.css'
 import News from './News/News'
-
+import { PreloaderSkateboardForComponent } from '../common/Preloaders/PreloaderSkateboard'
 
 
 const MyNews = (props) => {
+    if(!props.newsList){
+        return <PreloaderSkateboardForComponent/>
+    }
+
     return (
     <div className={c.MyNews}>
         {props.newsList.map(el => <News title={el.title} abstract={el.abstract} url={el.url}
